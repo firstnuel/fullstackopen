7e31fe6db6bef4ac1c94e1efe4102206abf43a9a
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-08-31 00:33:36 +0300

    Started working on exercise 4

---
+++ part4/blog-list/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blog')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+
+
+
+
+module.exports = app
