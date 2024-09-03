2d651b2830e75ef8d7869f542ab5e9d6d58d709c
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-09-03 11:14:40 +0300

    completed exercise 3 - fixed EsLint errors

--- index.js
+++ index.js
@@ -27,42 +27,41 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 app.get('/info', (req, res) => {
-    const d = new Date()
-    Person.find({}).then(persons => {
-      res.send(`<h3>Phonebook has info for ${persons.length} people<br><br>${d}</h3>`)
-    } )
-    
+  const d = new Date()
+  Person.find({}).then(persons => {
+    res.send(`<h3>Phonebook has info for ${persons.length} people<br><br>${d}</h3>`)
+  } )
 })
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
-      res.json(persons)
-      }
-    )}
+    res.json(persons)
+  }
+  )}
 )
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
+  Person.findById(req.params.id)
     .then(person => {
       console.log(person)
       if (person) {
         res.json(person)
       }else {
-      res.status(404).send('person not found') }
+        res.status(404).send('person not found') }
     })
     .catch(error => next(error))
-  })
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
@@ -79,15 +78,15 @@ app.delete('/api/persons/:id', (req, res, next) => {
 //   }
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if((!body.name) || (!body.number)) {
-        return res.status(400).json({ 
-            error: 'name or number is missing' 
-          })
-    }
+  if((!body.name) || (!body.number)) {
+    return res.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
 
-    Person.findOne({ name: body.name })
+  Person.findOne({ name: body.name })
     .then(personExists => {
       if(personExists){
         return res.status(400).json({
@@ -97,13 +96,13 @@ app.post('/api/persons', (req, res, next) => {
         const newPerson = new Person ({
           name : body.name,
           number : body.number
-      })
-      newPerson.save().then(response => 
-        res.json(response))
-        .catch(error => next(error))
+        })
+        newPerson.save().then(response =>
+          res.json(response))
+          .catch(error => next(error))
       }
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -112,8 +111,7 @@ app.put('/api/persons/:id', (request, response, next) => {
   const newPerson = {
     name : body.name,
     number : body.number
-
-}
+  }
   Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
@@ -126,5 +124,5 @@ app.use(errorHandler)
 
 const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
+  console.log('Server running on port', PORT)
 })
