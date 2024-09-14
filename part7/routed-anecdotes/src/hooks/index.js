cf5c4b624abf66a2fb7f7d4f04c96b80a1ebc5d1
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-09-14 17:51:27 +0300

    Completed Exercises 7.4 - 7.7

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+
+export const useField = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    name,
+    value,
+    onChange,
+    reset
+  }
+}
+
+
+
+// export const clearField = () => {
+
+// }
