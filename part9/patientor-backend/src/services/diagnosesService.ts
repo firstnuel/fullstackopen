d91c5ed8154e686d1316a7fd03ac2a1fe8a49b24
Emmanuel Ikwunna <99360864+firstnuel@users.noreply.github.com>
2024-11-22 13:59:18 +0200

    Delete part9/patientor-backend directory

--- part9/patientor-backend/src/services/diagnosesService.ts
+++
@@ -1,9 +0,0 @@
-import diagnosesData from '../../data/diagnoses';
-import { Diagnosis } from '../types';
-
-
-const getDiagnoses = (): Diagnosis[] => {
-    return diagnosesData;
-  };
-  
-export default getDiagnoses;
